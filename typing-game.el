be18205fe5cedf77c031251d51d9019375206f35
darksun <lujun9972@sina.com>
2015-10-27 08:37:16 +0800

    rename to typing-game

--- typing-game.el
+++ typing-game.el
@@ -1,60 +1,65 @@
-(defgroup type-game nil
+(defgroup typing-game nil
   "打字游戏")
-(defcustom type-game-height 10
+(defcustom typing-game-height 10
   "打字游戏界面的行数")
-(defcustom type-game-width 10
+(defcustom typing-game-width 10
   "打字游戏界面的列数")
-(defcustom type-game-letters "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+(defcustom typing-game-letters "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
   "字母候选列表")
 (defcustom letter-number-on-row 2
   "每行产生的新字母数")
 
-(defun type-game//random-letter ()
+(defun typing-game//random-letter ()
   ""
-  (elt type-game-letters (random (length type-game-letters))))
+  (elt typing-game-letters (random (length typing-game-letters))))
 
-(defun type-game//generate-letters ()
+(defun typing-game//generate-letters ()
   "返回新产生的字母"
-  (let ((str (make-string type-game-width ?\ )))
+  (let ((str (make-string typing-game-width ?\ )))
     (dotimes (var letter-number-on-row)
-      (setf (elt str (random type-game-width)) (type-game//random-letter)))
+      (setf (elt str (random typing-game-width)) (typing-game//random-letter)))
     str))
 
-(defun type-game/down (buffer)
+(defun typing-game/down (buffer)
   "字符下滚一行"
   (with-current-buffer buffer
     (save-excursion
       (let ((inhibit-read-only t))
         (goto-char (point-min))
-        (insert (type-game//generate-letters))
+        (insert (typing-game//generate-letters))
         (newline)
         ;; 跳转到第N行
         (goto-char (point-min))
-        (forward-line (- type-game-height 1))
+        (forward-line (- typing-game-height 1))
         ;; 删除后面的内容
         (end-of-line)
         (delete-region (point) (point-max))))))
 
-(defvar type-game-timer nil)
+(defvar typing-game-timer nil)
 
-(defun type-game/start-game-at-speed (speed)
+(defcustom typing-game-buffer "*typing-game*"
+  "buffer name of typing-game")
+
+(defun typing-game/start-game-at-speed (speed)
   ""
-  (interactive "N")
-  ;; (type-game-mode 1)
-  ;; (let ((height-delta (- type-game-height (window-body-height)))
-  ;;       (width-delta (- type-game-width (window-body-width))))
+  (interactive "P")
+  (setq speed (or speed 1))
+  (switch-to-buffer (get-buffer-create typing-game-buffer))
+  ;; (typing-game-mode 1)
+  ;; (let ((height-delta (- typing-game-height (window-body-height)))
+  ;;       (width-delta (- typing-game-width (window-body-width))))
   ;;   (window-resize nil width-delta nil t)
   ;;   (window-resize nil height-delta t t))
   ;; (setq window-size-fixed t)
-  (type-game/stop-game)
-  (setq type-game-timer (run-with-timer (/ 1.0 speed) (/ 5 speed) #'type-game/down (current-buffer))))
+  (typing-game/stop-game)
+  (setq typing-game-timer (run-with-timer (/ 1.0 speed) (/ 5 speed) #'typing-game/down (current-buffer))))
 
-(defun type-game/stop-game ()
+(defun typing-game/stop-game ()
   (interactive)
-  (when type-game-timer
-    (cancel-timer type-game-timer)))
+  (when typing-game-timer
+    (cancel-timer typing-game-timer)))
 
-(defun type-game/erase ()
+(defun typing-game/erase ()
   (interactive)
   (save-excursion
     (goto-char (point-min))
@@ -67,6 +72,6 @@
       (replace-string (this-command-keys)" "))))
 
 
-(define-derived-mode type-game-mode text-mode "type-game"
-  "Major mode for running type-game"
-  (local-set-key  [remap self-insert-command] 'type-game/erase))
+(define-derived-mode typing-game-mode text-mode "typing-game"
+  "Major mode for running typing-game"
+  (local-set-key  [remap self-insert-command] 'typing-game/erase))
