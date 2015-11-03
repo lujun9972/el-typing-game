d2b4c3395e0eed04ce91946f228268595de61e28
darksun <lujun9972@sina.com>
2015-11-03 20:35:25 +0800

    精简无用代码

--- typing-game.el
+++ typing-game.el
@@ -34,29 +34,26 @@
   (set-window-start (selected-window) (point-min))
   (goto-char (point-min)))
 
-(defun typing-game/scroll-down (&optional buffer)
+(defun typing-game/scroll-down ()
   "字幕下滚一行"
-  (let ((buffer (or buffer
-                    (current-buffer))))
-    (with-current-buffer buffer
-      (when (eq major-mode 'typing-game-mode) ;这样光标离开游戏窗口后,便不再继续
-        (save-excursion
-          (let ((inhibit-read-only t)
-                (typing-game-height (window-body-height)))
-            (goto-char (point-min))
-            (insert (typing-game//generate-letters typing-game-letters-per-row))
-            (newline)
-            ;; 跳转到第N行
-            (goto-char (point-min))
-            (forward-line (- typing-game-height 1))
-            (end-of-line)
-            ;; 删除后面的内容
-            (let* ((escaped-letters (replace-regexp-in-string "[ \r\n]" "" (buffer-substring-no-properties (point) (point-max))))
-                   (escaped-letter-number (length escaped-letters)))
-              (setq typing-game-escaped-letters (concat escaped-letters typing-game-escaped-letters))
-              (typing-game//change-scores (* typing-game-scores-per-escaped-letter escaped-letter-number)))
-            (delete-region (point) (point-max))))
-        (typing-game//fix-screen)))))
+  (when (eq major-mode 'typing-game-mode) ;这样光标离开游戏窗口后,便不再继续
+    (save-excursion
+      (let ((inhibit-read-only t)
+            (typing-game-height (window-body-height)))
+        (goto-char (point-min))
+        (insert (typing-game//generate-letters typing-game-letters-per-row))
+        (newline)
+        ;; 跳转到第N行
+        (goto-char (point-min))
+        (forward-line (- typing-game-height 1))
+        (end-of-line)
+        ;; 删除后面的内容
+        (let* ((escaped-letters (replace-regexp-in-string "[ \r\n]" "" (buffer-substring-no-properties (point) (point-max))))
+               (escaped-letter-number (length escaped-letters)))
+          (setq typing-game-escaped-letters (concat escaped-letters typing-game-escaped-letters))
+          (typing-game//change-scores (* typing-game-scores-per-escaped-letter escaped-letter-number)))
+        (delete-region (point) (point-max))))
+    (typing-game//fix-screen)))
 
 (defun typing-game/erase ()
   (interactive)
