8937cab6133bcdd46374da83aa19dc8b82cd400c
lujun9972 <lujun9972@gmail.com>
2015-11-05 20:21:53 +0800

    add comment

--- typing-game.el
+++ typing-game.el
@@ -1,3 +1,35 @@
+;;; typing-game.el --- a simple typing game in Emacs
+
+;; Copyright (C) 2004-2015 Free Software Foundation, Inc.
+
+;; Author: DarkSun <lujun9972@gmail.com>
+;; Keywords: lisp, game 
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Source code
+;;
+;; typing-game's code can be found here:
+;;   http://github.com/lujun9972/el-typing-game
+
+;;; Commentary:
+
+;; typing-game is a simple typing game in Emacs. You can customzie which characters to practice, how fast the characters failing and something else.
+
+;;
 (defgroup typing-game nil
   "typing game")
 (defcustom typing-game-characters "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
